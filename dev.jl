013cd297a70403bd3a0b1b62d37a757f2c30e06f
FedeClaudi <federicoclaudi@protonmail.com>
2022-09-07 09:28:56 -0400

    fast kernel

--- dev.jl
+++ dev.jl
@@ -30,8 +30,8 @@ mexicanhat(t) = α * 2/(√(3σ)*π^(1/4))*(1 - (t/σ)^2) * exp(-t^2/(2σ))
 k(x::Float64)::Float64 = mexicanhat(x)
 
 # construct network
-can = CAN(n, ξ_r, d, Kernel(k));
-show_connectivity(can) |> display
+# can = CAN(n, ξ_r, d, Kernel(k));
+# show_connectivity(can) |> display
 
 # ----------------------------------- torus ---------------------------------- #
 @info "doing torus"
@@ -49,14 +49,20 @@ end
 d = PeriodicEuclidean([2π, 2π])  # distance function over a torus manifold
 
 # connectivity kernel
-a = 1.0
-α = 0.10315
-l = 1.0
-λ = 13/2π
-β = 3/(λ^2)
-γ = 1.05 * β
 
-k(x::Float64)::Float64 = a * exp(-γ*abs(x)^2) - exp(-β*abs(x)^2)
+k(x::Float64)::Float64 = begin
+
+    a = 1.0
+    α = 0.10315
+    l = 1.0
+    λ = 13/2π
+    β = 3/(λ^2)
+    γ = 1.05 * β
+
+    
+    _x = abs(x)^2
+    a * exp(-γ*_x) - exp(-β*_x)
+end
 
 # construct network
 can = @time CAN(n, ξ_t, d, Kernel(k))
